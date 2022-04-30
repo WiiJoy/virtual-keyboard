89356e742982a06854a64b9312005114b392dee0
WiiJoy MacOS <wiijoy@bk.ru>
2022-04-30 13:58:41 +0300

    feat: out all buttons to screen

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,21 @@ import '../scss/style.scss'
 alert('Check alert!')
 console.log(buttons)
 
+const wrapper = document.createElement('div')
+
+
+for (let line of buttons.en) {
+    const btnLine = document.createElement('div')
+    for (let button of line) {
+        const btn = document.createElement('div')
+        btn.append(button.key)
+        btnLine.append(btn)
+    }
+    wrapper.append(btnLine)
+}
+
+document.body.append(wrapper)
+
 document.addEventListener('keyup', (ev) => {
     console.log(ev)
 })
