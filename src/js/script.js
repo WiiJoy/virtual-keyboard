8da28b21df57491171920758fd8132d4e67d4a7a
WiiJoy MacOS <wiijoy@bk.ru>
2022-05-01 16:20:29 +0300

    feat: pack render to class Keyboard

--- src/js/script.js
+++ src/js/script.js
@@ -3,33 +3,67 @@ import createElement from './modules/dom-create'
 
 import '../scss/style.scss'
 
-alert('Check alert!')
+// alert('Check alert!')
 // console.log(buttons)
 
-const textarea = createElement('textarea', document.body, 'keyboard__input')
-textarea.setAttribute('autofocus', '')
+class Keyboard {
+    constructor() {
+        this.buttons = {
+            ru: buttons.ru,
+            en: buttons.en
+        }
+        this.textarea = null
+        this.wrapper = null
+    }
 
-const wrapper = createElement('div', document.body, 'keyboard__wrapper')
+    init() {
+        this.textarea = createElement('textarea', document.body, 'keyboard__input')
+        this.textarea.setAttribute('autofocus', '')
+
+        this.wrapper = createElement('div', document.body, 'keyboard__wrapper')
+
+        for (let line of this.buttons.ru) {
+            const btnLine = createElement('div', this.wrapper, 'keyboard__line')
+            for (let button of line) {
+                // console.log(button)
+                const btnClasses = ['keyboard__btn']
+        
+                if (button.keyClasses) btnClasses.push(button.keyClasses)
+        
+                const btn = createElement('div', btnLine, ...btnClasses)
+                btn.innerHTML = button.key
+                // btnLine.append(btn)
+            }
+            // wrapper.append(btnLine)
+        }
+    }
+}
+
+// const textarea = createElement('textarea', document.body, 'keyboard__input')
+// textarea.setAttribute('autofocus', '')
+
+// const wrapper = createElement('div', document.body, 'keyboard__wrapper')
 
 // const wrapper = document.createElement('div')
 
 
-for (let line of buttons.ru) {
-    const btnLine = createElement('div', wrapper, 'keyboard__line')
-    for (let button of line) {
-        // console.log(button)
-        const btnClasses = ['keyboard__btn']
+// for (let line of this.buttons.ru) {
+//     const btnLine = createElement('div', this.wrapper, 'keyboard__line')
+//     for (let button of line) {
+//         // console.log(button)
+//         const btnClasses = ['keyboard__btn']
 
-        if (button.keyClasses) btnClasses.push(button.keyClasses)
+//         if (button.keyClasses) btnClasses.push(button.keyClasses)
 
-        const btn = createElement('div', btnLine, ...btnClasses)
-        btn.innerHTML = button.key
-        // btnLine.append(btn)
-    }
-    // wrapper.append(btnLine)
-}
+//         const btn = createElement('div', btnLine, ...btnClasses)
+//         btn.innerHTML = button.key
+//         // btnLine.append(btn)
+//     }
+//     // wrapper.append(btnLine)
+// }
 
-
+const keyboard = new Keyboard()
+keyboard.init()
 
 // document.body.append(wrapper)
 
